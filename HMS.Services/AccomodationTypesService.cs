ccebae6d122e39b0ca7bff5dae9d51323c5bdd30
abhinav narwariya <abhinavhhh18@gmail.com>
2020-05-21 00:36:04 +0530

    created listiong of accomodationtype

---
+++ HMS.Services/AccomodationTypesService.cs
@@ -0,0 +1,24 @@
+﻿using HMS.Data;
+using HMS.Entities;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace HMS.Services
+{
+   public class AccomodationTypesService
+    {
+        public IEnumerable<AccomodationType> GetAllAccomodationType()
+        {
+            HMSContext Context = new HMSContext();
+
+            var  data = Context.AccomodationTypes.ToList();
+
+            return data;
+        }
+
+
+    }
+}
---
+++ HMS/Areas/Dashboard/Controllers/AccomodationTypesController.cs
@@ -0,0 +1,31 @@
+﻿using HMS.Areas.Dashboard.ViewModel;
+using HMS.Services;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace HMS.Areas.Dashboard.Controllers
+{
+    public class AccomodationTypesController : Controller
+    {
+
+        AccomodationTypesService objAccomodationTypesService = new AccomodationTypesService();
+        // GET: Dashboard/AccomodatioTypes
+        public ActionResult Index()
+        {
+            return View();
+        }
+
+
+        public ActionResult Listing()
+        {
+            AccomodationTypeViewModel objAccomodationTypeViewModel = new AccomodationTypeViewModel();
+            objAccomodationTypeViewModel.AccomodationTypes = objAccomodationTypesService.GetAllAccomodationType();
+
+            return PartialView("_Listing", objAccomodationTypeViewModel);
+        }
+
+    }
+}
